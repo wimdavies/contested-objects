041968c6b9683279060f4cef8830f38f6bd3e193
wimdavies <william.johnston.davies@gmail.com>
2024-02-07 14:22:15 +0000

    Add format validation to MuseumObject system_number

--- app/models/museum_object.rb
+++ app/models/museum_object.rb
@@ -1,3 +1,3 @@
 class MuseumObject < ApplicationRecord
-  validates :system_number, presence: true, uniqueness: true
+  validates :system_number, presence: true, uniqueness: true, format: { with: /\AO\d+\z/ , message: "must be in correct format"}
 end
