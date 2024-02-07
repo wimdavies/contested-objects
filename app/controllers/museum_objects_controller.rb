2488f84b3349d8aa92c3bed033c700de4ebb5eea
wimdavies <william.johnston.davies@gmail.com>
2024-02-07 13:28:04 +0000

    Add save object form button to search result

--- app/controllers/museum_objects_controller.rb
+++ app/controllers/museum_objects_controller.rb
@@ -10,6 +10,8 @@ class MuseumObjectsController < ApplicationController
   end
   
   def search
+    @museum_object = MuseumObject.new
+
     response = HTTParty.get("https://api.vam.ac.uk/v2/museumobject/#{params[:system_number]}", format: :plain)
 
     if response.success?
@@ -21,23 +23,20 @@ class MuseumObjectsController < ApplicationController
     end
   end
 
-  # def new
-  #   @museum_object = MuseumObject.new
-  # end
+  def create
+    @museum_object = MuseumObject.new(museum_object_params)
 
-  # # def create
-  #   @museum_object = MuseumObject.new(museum_object_params)
+    if @museum_object.save
+      redirect_to root_path, notice: "Object saved successfully", status: :see_other
+    else
+      flash.now[:alert] = "Object could not be saved"
+      render :search, status: :unprocessable_entity
+    end
+  end
 
-  #   if @museum_object.save
-  #     redirect_to @museum_object
-  #   else
-  #     render :new, status: :unprocessable_entity
-  #   end
-  # end
+  private
 
-  # private
-
-  # def museum_object_params
-  #   params.require(:museum_object).permit(:system_number)
-  # end
+  def museum_object_params
+    params.require(:museum_object).permit(:system_number)
+  end
 end
