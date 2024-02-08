afa9a07ec8dc553268102bd45da567f9694bd5d7
wimdavies <william.johnston.davies@gmail.com>
2024-02-08 12:19:06 +0000

    Configure vcr integration into RSpec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,15 @@
 # it.
 #
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+require 'vcr'
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/cassettes'
+  c.hook_into :webmock
+  c.configure_rspec_metadata!
+end
+
 RSpec.configure do |config|
   # config.success_color = :blue
   
