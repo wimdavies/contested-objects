d4b98c180c6dd87a11d9b6da88b7efdc6edf2810
wimdavies <william.johnston.davies@gmail.com>
2024-02-08 18:22:43 +0000

    TDD: VandaClient base_uri, .retireve_object_record returns status 200 for valid system_number

--- lib/clients/vanda_client.rb
+++ lib/clients/vanda_client.rb
@@ -1,5 +1,10 @@
-require 'httparty'
+require "httparty"
 
 class VandaClient
   include HTTParty
+  base_uri "https://api.vam.ac.uk/v2"
+
+  def self.retrieve_object_record(system_number)
+    self.get("/museumobject/#{system_number}")
+  end
 end
--- spec/lib/vanda_client_spec.rb
+++ spec/lib/vanda_client_spec.rb
@@ -4,4 +4,15 @@ describe VandaClient, :vcr do
   it "includes HTTParty" do
     expect(VandaClient.ancestors).to include HTTParty
   end
+
+  it "sets the base_uri correctly" do
+    expect(VandaClient.base_uri).to eq "https://api.vam.ac.uk/v2"
+  end
+
+  describe ".retrieve_object_record" do
+    it "returns status 200 OK for a valid system_number" do
+      response = VandaClient.retrieve_object_record("O108443")
+      expect(response.success?).to eq true
+    end
+  end
 end
