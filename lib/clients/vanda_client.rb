5a217540a3770b0d1cf1081edacc7b3287262353
wimdavies <william.johnston.davies@gmail.com>
2024-02-08 13:29:15 +0000

    Add initial test and blank class for VandaClient

---
+++ lib/clients/vanda_client.rb
@@ -0,0 +1,5 @@
+class VandaClient
+  # def initialize
+    
+  # end
+end
---
+++ spec/lib/vanda_client_spec.rb
@@ -0,0 +1,10 @@
+require 'clients/vanda_client'
+
+describe VandaClient, :vcr do
+  describe "" do
+    it "" do
+      
+      expect(true).to eq(true)
+    end
+  end
+end
