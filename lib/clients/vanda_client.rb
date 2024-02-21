5c7fe10f417552b6182b71265e32f516fe0670c4
wimdavies <william.johnston.davies@gmail.com>
2024-02-21 17:03:35 +0000

    Add basic version of API client. Parsing should be extracted into a custom parser. TODO: error handling

--- lib/clients/vanda_client.rb
+++ lib/clients/vanda_client.rb
@@ -1,10 +1,15 @@
 require "httparty"
+require "active_support"
+require "active_support/core_ext/hash/keys"
+require "active_support/core_ext/string/inflections"
 
 class VandaClient
   include HTTParty
   base_uri "https://api.vam.ac.uk/v2"
+  default_timeout 10
 
-  def self.retrieve_object_record(system_number)
-    self.get("/museumobject/#{system_number}")
+  def self.retrieve_single_object_record(system_number)
+    response = self.get("/museumobject/#{system_number}")
+    JSON.parse(response.parsed_response.deep_transform_keys(&:underscore).to_json, object_class: OpenStruct)
   end
 end
