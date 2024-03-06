81dc2fb3590c302b43ab2d7ca12809a6b868f1f1
wimdavies <william.johnston.davies@gmail.com>
2024-03-06 13:25:07 +0000

    TDD VandaClient returning status codes and raising custom ArgumentError for invalid system_number

--- spec/lib/vanda_client_spec.rb
+++ spec/lib/vanda_client_spec.rb
@@ -10,9 +10,24 @@ describe VandaClient, :vcr do
   end
 
   describe ".retrieve_single_object_record" do
-    it "returns an HTTParty response object when 200 OK" do
+    it "returns an HTTParty response object for a valid system number" do
       response = VandaClient.retrieve_single_object_record("O108443")
       expect(response.class).to eq HTTParty::Response
+    end
+    
+    it "raises custom ArgumentError if passed an invalid system number" do
+      expect { VandaClient.retrieve_single_object_record("bananas") }
+        .to raise_error(an_instance_of(ArgumentError).and having_attributes({"message" => "System number must be in valid format"}))
+    end
+
+    it "returns status 200 OK for valid existing record" do
+      response = VandaClient.retrieve_single_object_record("O108443")
+      expect(response.ok?).to eq true
+    end
+
+    it "returns status 404 for valid, non-extant record" do
+      response = VandaClient.retrieve_single_object_record("O99999999")
+      expect(response.not_found?).to eq true
     end
   end
 end
