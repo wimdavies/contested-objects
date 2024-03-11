9f7af64102c1af89024013a16dd4759e55f3c6de
wimdavies <william.johnston.davies@gmail.com>
2024-03-11 16:03:36 +0000

    Separate VandaCollection classes into their own files

--- lib/vanda_collection/client.rb
+++ lib/vanda_collection/client.rb
@@ -2,7 +2,6 @@ require "httparty"
 require "active_support"
 require "active_support/core_ext/hash/keys"
 require "active_support/core_ext/string/inflections"
-require "ostruct"
 
 module VandaCollection
   class Client
@@ -15,21 +14,6 @@ module VandaCollection
       raise ArgumentError.new("System number must be in valid format") unless system_number =~ SYSTEM_NUMBER_FORMAT
   
       self.get("/museumobject/#{system_number}")
-    end
-  end
-
-  class Wrapper
-    def self.find_by_system_number(system_number)
-      response = VandaCollection::Client.retrieve_single_object_record(system_number)
-
-      if response.success?
-        JSON.parse(response.deep_transform_keys(&:underscore).to_json, object_class: OpenStruct)
-      else
-        # current plan is to return nil for status code other than 200 OK
-        nil
-      end
-    # rescue
-    #   # error handling goes here
     end
   end
 end
---
+++ lib/vanda_collection/wrapper.rb
@@ -0,0 +1,18 @@
+require "ostruct"
+
+module VandaCollection
+  class Wrapper
+    def self.find_by_system_number(system_number)
+      response = VandaCollection::Client.retrieve_single_object_record(system_number)
+
+      if response.success?
+        JSON.parse(response.deep_transform_keys(&:underscore).to_json, object_class: OpenStruct)
+      else
+        # current plan is to return nil for status code other than 200 OK
+        nil
+      end
+    # rescue
+    #   # error handling goes here
+    end
+  end
+end
