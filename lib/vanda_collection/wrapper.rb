e6a28539c3736b3475cd33899c5997a4d17aacef
wimdavies <william.johnston.davies@gmail.com>
2024-03-13 11:51:02 +0000

    Correct Wrapper private method definition. Comment out Wrapper's client response code coupling

--- lib/vanda_collection/wrapper.rb
+++ lib/vanda_collection/wrapper.rb
@@ -6,20 +6,21 @@ module VandaCollection
     def self.find_by_system_number(system_number)
       response = VandaCollection::Client.retrieve_single_object_record(system_number)
 
-      if response.success?
+      # if response.success?
         parse_single_object_record(response)
-      else
+      # else
         # current plan is to return nil for status code other than 200 OK
-        nil
-      end
+        # nil
+      # end
     # rescue
     #   # error handling goes here
     end
 
-    private
-
-    def parse_single_object_record(response)
-      JSON.parse(response.deep_transform_keys(&:underscore).to_json, object_class: OpenStruct)
+    private_class_method def self.parse_single_object_record(response)
+      JSON.parse(
+        response.deep_transform_keys(&:underscore).to_json,
+        object_class: OpenStruct
+      )
     end
   end
 end
