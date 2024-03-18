b0766f12d614c965a748970a104cca0879ae9d74
wimdavies <william.johnston.davies@gmail.com>
2024-03-18 17:18:46 +0000

    Add password edit functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,14 +6,18 @@ class ApplicationController < ActionController::Base
   end
   helper_method :current_user
 
-  def authenticate_user_from_session
-    User.find_by(id: session[:user_id])
-  end
-
   def user_signed_in?
     current_user.present?
   end
   helper_method :user_signed_in?
+
+  def authenticate_user!
+    redirect_to root_path, alert: "You must be logged in to do that." unless user_signed_in?
+  end
+
+  def authenticate_user_from_session
+    User.find_by(id: session[:user_id])
+  end
 
   def login(user)
     Current.user = user
