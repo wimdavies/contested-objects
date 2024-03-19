20a2c1b17072a0b5eeaa3672710dff1baa4ca681
wimdavies <william.johnston.davies@gmail.com>
2024-03-19 18:26:02 +0000

    Add custom validation to Reflection for at least one text field prompt response

--- app/models/reflection.rb
+++ app/models/reflection.rb
@@ -3,4 +3,12 @@ class Reflection < ApplicationRecord
   belongs_to :museum_object
 
   validates :deaccession_flag, presence: true, inclusion: [true, false]
+  validate :at_least_one_text_response_required
+
+  def at_least_one_text_response_required
+    text_fields = [deaccession_reason, provenance_thoughts, presence_feelings, absence_feelings]
+    if text_fields.all?(&:blank?)
+      errors.add :base, :text_response, message: "You must write a response to at least one prompt"
+    end
+  end
 end
