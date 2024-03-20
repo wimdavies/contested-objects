3b01327fecf056f582220711edfbf326f0e20095
wimdavies <william.johnston.davies@gmail.com>
2024-03-20 18:48:33 +0000

    Complete basic implementation of reflections#create

--- app/controllers/reflections_controller.rb
+++ app/controllers/reflections_controller.rb
@@ -1,13 +1,27 @@
 class ReflectionsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
   end
 
   def new
-    @reflection = Reflection.new
+    @museum_object = MuseumObject.find_by!(system_number: params[:system_number])
     @vanda_object = VandaCollection::Wrapper.find_by_system_number(params[:system_number])
+    @reflection = @museum_object.reflections.new
   end
 
   def create
+    @museum_object = MuseumObject.find_by!(system_number: params[:system_number])
+    @vanda_object = VandaCollection::Wrapper.find_by_system_number(params[:system_number])
+    @reflection = @museum_object.reflections.new(reflection_params)
+    @reflection.update(user_id: current_user.id)
+
+    if @reflection.save
+      redirect_to @reflection, notice: "Reflection saved successfully!"
+    else
+      flash.now[:alert] = "Reflection could not be saved."
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -20,5 +34,17 @@ class ReflectionsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def reflection_params
+    params.require(:reflection).permit(
+      :deaccession_flag,
+      :deaccession_reason,
+      :provenance_thoughts,
+      :presence_feelings,
+      :absence_feelings
+    )
   end
 end
